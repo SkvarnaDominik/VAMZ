0946498bfd8985369819be544227bfd96c3ba4b5
Dominik Škvarna <skvarnad@gmail.com>
2023-05-05 16:22:12 +0200

    0000

--- app/src/main/java/com/example/semestralnapraca_skvarna/database/GameRecordDatabase.kt
+++ app/src/main/java/com/example/semestralnapraca_skvarna/database/GameRecordDatabase.kt
@@ -1,4 +1,31 @@
 package com.example.semestralnapraca_skvarna.database
 
-class GameRecordDatabase {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [GameRecord::class], version = 1, exportSchema = false)
+abstract class GameRecordDatabase : RoomDatabase() {
+
+    abstract fun gameRecordDao(): GameRecordDao
+
+    companion object {
+
+    @Volatile
+    private var INSTANCE: GameRecordDatabase? = null
+
+    fun getDatabase(context: Context): GameRecordDatabase {
+        val tempInstance = INSTANCE
+        if (tempInstance != null) {
+            return tempInstance
+        }
+        synchronized(this) {
+            val instance = Room.databaseBuilder(context.applicationContext, GameRecordDatabase::class.java, "game_records").build()
+            INSTANCE = instance
+            return instance
+        }
+    }
+    }
+
 }
