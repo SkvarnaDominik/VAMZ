c04be3c5d0837964538ed96c2152f003e650f54e
Dominik Škvarna <skvarnad@gmail.com>
2023-05-09 16:49:37 +0200

    0000

--- app/src/main/java/com/example/semestralnapraca_skvarna/fragment/TextColorFragment.kt
+++ app/src/main/java/com/example/semestralnapraca_skvarna/fragment/TextColorFragment.kt
@@ -27,7 +27,7 @@ class TextColorFragment : Fragment(R.layout.fragment_text_color) {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentTextColorBinding.inflate(inflater, container, false)
+        _binding = FragmentTextColorBinding.inflate(inflater, container, false) //Nastavenie hodnôt premenným
         viewModel = ViewModelProvider(this)[TextColorViewModel::class.java]
 
         return binding.root
@@ -36,106 +36,105 @@ class TextColorFragment : Fragment(R.layout.fragment_text_color) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.btnYellow.setTextColor(Color.BLACK)
+        binding.btnYellow.setTextColor(Color.BLACK) //Nastavenie farby textu v textView
         binding.btnBlue.setTextColor(Color.BLACK)
         binding.btnRed.setTextColor(Color.BLACK)
         binding.btnGreen.setTextColor(Color.BLACK)
 
-        binding.tvScore.text = sharedViewModel.getScore().toString()
-
-        setOnClickBtnYellow()
+        setOnClickBtnYellow() //ClickListener pre stlačenie tlačidla
         setOnClickBtnBlue()
         setOnClickBtnRed()
         setOnClickBtnGreen()
         setOnClickBackToMenu()
 
-        timerSetup()
-        displaySeconds()
-        isFinished()
-        gameSetup()
+        timerSetup() //Metóda pre výber dĺžky Timer-u
+        displaySeconds() //Zobrazenie sekúnd
+        isFinished() //Kontrola skončenia Timer-u
+        roundSetup() //Metóda pre nastavenie daného kola
     }
 
-    private fun timerSetup(){
-        if (sharedViewModel.getDifficulty() == "Easy") {
-          viewModel.startTimer(5)
+    private fun timerSetup() { //Metóda pre výber dĺžky Timer-u
+        if (sharedViewModel.getDifficulty() == "Easy") { //Ak je zvolený mód Easy, odpočítavať sa bude od 5 sekúnd
+          viewModel.startTimer(5) //Nastavenie dĺžky timer-u na 5 sekund
           return
         }
-        viewModel.startTimer(3)
+        viewModel.startTimer(3) //Ak je zvolený mód Hard, odpočítavať sa bude od 3 sekúnd
     }
 
-    private fun gameSetup() {
-        viewModel.shuffleModes()
-        viewModel.setMode(viewModel.getModes()[0])
-        binding.tvMode.text = viewModel.getMode()
+    private fun roundSetup() { //Metóda pre nastavenie daného kola
+        viewModel.shuffleModes() //Metóda pre zamiešanie poľa s módamy výberu tlačidla
+        viewModel.setMode(viewModel.getModes()[0]) //Nastavenie módu výberu tlačidla pre dané kolo
+        binding.tvMode.text = viewModel.getMode() //Vypísanie výberu módu tlačidla do textView tvMode
 
-        viewModel.shuffleTextColors()
-        viewModel.setColor(viewModel.getTextColors()[0])
-        binding.tvColor.text = viewModel.getTextColor()
+        viewModel.shuffleTextColors() //Metóda pre zamiešanie poľa s textovou reprezentaciou farieb, ktoré reprezentujú text v tlačidlách
+        viewModel.setColor(viewModel.getTextColors()[0]) //Nastavenie vybranej farby pre dané kolo
+        binding.tvColor.text = viewModel.getTextColor() //Vypísanie výberu farby do textView tvColor
 
-        viewModel.shuffleTextColors()
-        viewModel.shuffleColorButtons()
+        viewModel.shuffleTextColors() //Metóda pre zamiešanie poľa s textovou reprezentaciou farieb, ktoré reprezentujú text v tlačidlách
+        viewModel.shuffleColorButtons() //Metóda pre zamiešanie poľa s pozadiami tlačidiel, ktoré reprezentujú ich farbu
 
-        setupColorButtons()
+        setupColorButtons() //Nastavenie pozadí tlačidlám poďľa ich zamiešania
 
-        viewModel.setWinningColorButton(determinateWinningButton())
+        viewModel.setWinningColorButton(determinateWinningButton()) //Zistenie a zapísanie výherného tlačidla pre dané kolo do zdieľaného viewModel-u
     }
-   private fun game(pColorButtonNumber:Int) {
-            isSameAsChosen(determinateWinningButton(), pColorButtonNumber)
-            if (viewModel.getIsSame()) {
-               viewModel.restartTimer()
-                sharedViewModel.addScore()
-                binding.tvScore.text = sharedViewModel.getScore().toString()
-                gameSetup()
+   private fun game(pColorButtonNumber:Int) { //Metóda logiky hry
+            isSameAsChosen(determinateWinningButton(), pColorButtonNumber) //Metóda pre zistenie, či používateľ stlačil výherné tlaćidlo
+            if (viewModel.getIsSame()) { //Podmienka, ktorá vráti pravdu, ak sa výherné tlaćidlo rovná s použivateľovým vstupov
+                viewModel.restartTimer() //Resetovanie časovača na začiatočný čas (Easy = 5 sekund, Hard = 3 sekundy)
+                sharedViewModel.addScore() //Navýšenie skóre
+                binding.tvScore.text = sharedViewModel.getScore().toString() //Výpis skóre do textView tvScore
+                roundSetup() //Metóda pre nastavenie daného kola
             }
             else {
                 Navigation.findNavController(binding.root).navigate(R.id.action_textColorFragment_to_gameOverviewFragment)
             }
     }
 
-    private fun determinateWinningButton(): Int {
-        if (viewModel.getMode() == "Text") {
+    private fun determinateWinningButton(): Int { //Metóda pre zistenie výherného tlačidla
+        if (viewModel.getMode() == "Text") { //Podmienka pre zistenie herného módu daného kola
             for (index in 0 until viewModel.getTextColors().size) {
-                if (viewModel.getTextColors()[index] == viewModel.getTextColor())
-                    return index
+                if (viewModel.getTextColors()[index] == viewModel.getTextColor()) //Porovnávanie poľa textu farieb, kým sa nenájde index, ktorého hodnota sa rovná vybranej farbe
+                    return index //Vrátenie výherného indexu
             }
         }
        else {
-           when (viewModel.getTextColor()) {
+           when (viewModel.getTextColor()) { //Switch pre textové farby
                "Yellow" -> {
                    for (index in 0 until viewModel.getColorButtons().size) {
                        if (viewModel.getColorButtons()[index].toString() == "2131165328") //Toto je (R.drawable.btn_yellow).toString()
-                           return index
+                                                                                          //Porovnávanie poľa pozadí tlačidiel, až kým sa nenájde index, ktorého hodnota sa rovná vybranej farbe
+                           return index //Vrátenie výherného indexu
                    }
                }
 
                "Blue" -> {
                    for (index in 0 until viewModel.getColorButtons().size) {
-                       if (viewModel.getColorButtons()[index].toString() == "2131165309")
+                       if (viewModel.getColorButtons()[index].toString() == "2131165309") //Toto je (R.drawable.btn_blue).toString()
                            return index
                    }
                }
 
                "Red" -> {
                    for (index in 0 until viewModel.getColorButtons().size) {
-                       if (viewModel.getColorButtons()[index].toString() == "2131165325")
+                       if (viewModel.getColorButtons()[index].toString() == "2131165325") //Toto je (R.drawable.btn_red).toString()
                            return index
                    }
                }
 
                "Green" -> {
                    for (index in 0 until viewModel.getColorButtons().size) {
-                       if (viewModel.getColorButtons()[index].toString() == "2131165318")
+                       if (viewModel.getColorButtons()[index].toString() == "2131165318") //Toto je (R.drawable.btn_green).toString()
                            return index
                    }
                }
            }
         }
-        return -1
+        return -1 //V prípade chyby sa vráti -1
     }
 
-    private fun setupColorButtons() {
-        binding.btnYellow.setBackgroundResource(viewModel.getColorButtons()[0])
-        binding.btnYellow.text = viewModel.getTextColors()[0]
+    private fun setupColorButtons() { //Nastavenie pozadí tlačidlám poďľa ich zamiešania
+        binding.btnYellow.setBackgroundResource(viewModel.getColorButtons()[0]) //Nastavenie pozadia tlačidla na n-tý prvok z poľa pozadí tlačidiel
+        binding.btnYellow.text = viewModel.getTextColors()[0] //Nastavenie textu tlačidla na n-tý prvok z poľa textu farieb
         binding.btnBlue.setBackgroundResource(viewModel.getColorButtons()[1])
         binding.btnBlue.text = viewModel.getTextColors()[1]
         binding.btnRed.setBackgroundResource(viewModel.getColorButtons()[2])
@@ -144,56 +143,51 @@ class TextColorFragment : Fragment(R.layout.fragment_text_color) {
         binding.btnGreen.text = viewModel.getTextColors()[3]
     }
 
-    private fun isSameAsChosen(pWinningButtonNumber: Int,pColorButtonNumber: Int) {
+    private fun isSameAsChosen(pWinningButtonNumber: Int,pColorButtonNumber: Int) { //Metóda pre zistenie, či používateľ stlačil výherné tlaćidlo
         if (pWinningButtonNumber != pColorButtonNumber)
-            viewModel.setIsSame(false)
+            viewModel.setIsSame(false) //Ak sa použivateľov vstup nerovna s výherným tlačidlom, nastavý sa flag o rovnosti na false
     }
 
-    private fun displaySeconds() {
+    private fun displaySeconds() { //Zobrazenie sekúnd
         viewModel.getSeconds().observe(viewLifecycleOwner, Observer {
             binding.tvSeconds.text = (it.inc()  ).toString()
         })
     }
 
-    private fun isFinished() {
+    private fun isFinished() { //Kontrola skončenia Timer-u
         viewModel.getIsFinished().observe(viewLifecycleOwner, Observer {
             Navigation.findNavController(binding.root).navigate(R.id.action_textColorFragment_to_gameOverviewFragment)
         })
     }
 
-   private fun setOnClickBtnYellow() {
+   private fun setOnClickBtnYellow() { //ClickListener pre stlačenie tlačidla
         binding.btnYellow.setOnClickListener() {
            game(0 )
         }
     }
 
-    private fun setOnClickBtnBlue() {
+    private fun setOnClickBtnBlue() { //ClickListener pre stlačenie tlačidla
         binding.btnBlue.setOnClickListener() {
            game(1)
         }
     }
 
-    private fun setOnClickBtnRed() {
+    private fun setOnClickBtnRed() { //ClickListener pre stlačenie tlačidla
         binding.btnRed.setOnClickListener() {
             game(2)
         }
     }
 
-    private fun setOnClickBtnGreen() {
+    private fun setOnClickBtnGreen() { //ClickListener pre stlačenie tlačidla
         binding.btnGreen.setOnClickListener() {
             game(3)
         }
     }
 
-    private fun setOnClickBackToMenu() {
+    private fun setOnClickBackToMenu() { //ClickListener pre stlačenie tlačidla
         binding.btnBackToMenu.setOnClickListener() {
             sharedViewModel.resetScore()
             Navigation.findNavController(binding.root).navigate(R.id.action_textColorFragment_to_mainMenuFragment)
         }
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        _binding = null
     }
 }
